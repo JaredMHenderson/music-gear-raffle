ce5a09880a712c3427391fb2895eeae74ec0590a
Jared Henderson <jaredmhenderson@icloud.com>
2018-03-31 14:05:54 -0600

    intitial commit

---
+++ gear-raffle/src/main/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+render() {
+    return (
+        <div>
+            HELLOW
+            </div>
+    );
+}
+}
+
+export default Main;
