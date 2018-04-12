2d12715b560739ed157e635e5461c36112cb8601
cataire <mail.for.mu@gmail.com>
2018-04-12 13:19:05 -0600

    ViewItem component added and working. Needs styling

--- gear-raffle/src/App.js
+++ gear-raffle/src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from './main/Main';
 import Navbar from './nav/nav'
 import RaffleList from './raffle-list/raffle-list';
+import ViewItem from './view-item/view-item';
+import createBrowserHistory from 'history/createBrowserHistory';
 
-class App extends Component 
+class App extends Component
 {
-  constructor(props) 
+  constructor(props)
   {
     super(props);
-  
+
     this.state = {
        "raffleItems":[{
          imageUrl: 'https://media.sweetwater.com/api/i/q-82__ha-3d0905afc12f193c__hmac-d8d5a6ef1d7361381c0b3dca237102cb195bf41e/images/items/750/LB417-large.jpg',
@@ -20,7 +22,8 @@ class App extends Component
           ticketPrice: 10,
        }]
     }
-    this.addRaffle = this.addRaffle.bind(this);
+    this.addRaffle = this.addRaffle.bind(this)
+      this.history = createBrowserHistory();
   }
 
   addRaffle(raffle)
@@ -28,15 +31,17 @@ class App extends Component
     const raffleItems = [...this.state.raffleItems];
     raffleItems.push({raffleItems});
   }
-  
+
   render() {
     return (
-      <Router>
+      <Router history={this.history}>
       <div>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={() => <RaffleList raffles={this.state.raffleItems} />} />
+          <Route exact path='/' component={() => <RaffleList raffles={this.state.raffleItems} history={this.history} />} />
             <Route exact path='/main' component={Main} />
+            <Route path='/item' component={() => <ViewItem history={this.history} />} />
+
             {/* <Route exact path='/login' component={Login} /> */}
             {/* <Route exact path='/signout' component={Signout} /> */}
           </Switch>
@@ -49,5 +54,6 @@ class App extends Component
     );
   }
 }
+//pull from database
 
 export default App;
--- gear-raffle/src/raffle-card/raffle-card.js
+++ gear-raffle/src/raffle-card/raffle-card.js
@@ -1,32 +1,55 @@
 import React, { Component } from 'react'
 import './raffle-card.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import $ from 'jquery';
 
 
 
-export default class RaffleCard extends Component 
+
+export default class RaffleCard extends Component
 {
-    // constructor(props) 
-    // {
-    //   super(props)   
-    // }
+    constructor(props)
+    {
+      super(props);
 
-    render() 
+      this._getItem = this._getItem.bind(this);
+      this.state = {
+        goToItem: false,
+        itemId: null
+      };
+    }
+
+      _getItem(event) {
+
+        console.log("Clicked!!");
+        let itemId = event.target.attributes["data-id"].value;
+        console.log("These are props from card:", itemId);
+        console.log(this.props);
+        this.setState({itemId, goToItem: true});
+          }
+
+
+
+    render()
     {
         const {imageUrl, itemName, condition, ticketPrice} = this.props.raffle;
+
+        if(this.state.goToItem)
+        {
+          return <Redirect to={`/item/${this.state.itemId}`}/>;
+        }
+
         return (
             <div className="card">
                 <img className="card-img-top" src={imageUrl} alt="Item Pic"/>
                     <div className="card-body">
                     <h5 className="card-title"><span>Item:</span>{itemName}</h5>
                     <p><span>Condition:</span>{condition}</p>
-                    <p><span>Ticket Price: $</span>{ticketPrice}</p> 
-                        <a href="#" className="btn btn-primary">Buy Ticket</a>
+                    <p><span>Ticket Price: $</span>{ticketPrice}</p>
+                        <button onClick={this._getItem} className="btn btn-primary" data-id="5ace2f8cf378a265d8f84222">Buy Ticket</button>
                     </div>
             </div>
-            
+
         );
     }
 }
-    
-  
-
