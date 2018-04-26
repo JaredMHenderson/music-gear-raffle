cbb02e274d7bf5aaff72e87748bac556e6ad7241
Nick Moore <nickmoore04@gmail.com>
2018-04-25 19:09:24 -0600

    all up to date

--- gear-raffle/src/view-item/view-item.js
+++ gear-raffle/src/view-item/view-item.js
@@ -1,71 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './view-item.css';
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 import keys from '../config/keys.js';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Moment from 'react-moment';
-
 Moment.globalFormat = 'MMMM Do YYYY';
-
-
-class ViewItem extends Component
-{
-    constructor(props)
-    {
-      super(props);
-      this.state = {
-        imageUrl: null,
-        itemName: null,
-        condition: null,
-        ticketPrice: null,
-        itemId: null,
-        name: null,
-        email: null,
-        raffleStartDate: null,
-        raffleEndDate: null
-      }
-
-
+class ViewItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrl: null,
+      itemName: null,
+      condition: null,
+      ticketPrice: null,
+      itemId: null,
+      name: null,
+      email: null,
+      raffleStartDate: null,
+      raffleEndDate: null
     }
-
-
   }
-
-
-
-
-
   onToken(token) {
     console.log(token);
     console.log(token.email);
     let idFromPath = window.location.pathname.split("/");
     let itemId = idFromPath[2];
     console.log("here is id: ", itemId);
-
     const userData = token;
     axios.post('/api/stripe', { ...token, price: this.props.raffle.ticketPrice * 100 }).then(response => {
       console.log('\n \n \n \n \n \n \nLookie the response', response);
       console.log(`We are in business, ${response.email}`);
-
     });
-
     axios.post('/api/raffleItem/participant/' + itemId, { ...token, email: token.email }).then(response => {
       console.log('Success', response);
-
     });
-
-
   };
-
-
-
   componentWillMount() {
-
     let idFromPath = window.location.pathname.split("/");
     let itemId = idFromPath[2];
     console.log("here is id: ", itemId);
-
     const currentState = this.state;
     console.log("I'm about to mount");
     axios.get("/api/raffleItem/" + itemId)
@@ -87,25 +61,11 @@ class ViewItem extends Component
           if (error) {
             console.log(error);
           }
-
-
         }
       )
   }
-
-
-
-
-
-
-
-
-
-
   render() {
     const { itemName, imageUrl, ticketPrice, condition, raffleStartDate, raffleEndDate, itemId } = this.state;
-
-
     // if(itemName && imageUrl && ticketPrice && condition) {
     return (
       <div>
@@ -132,40 +92,18 @@ class ViewItem extends Component
                 <p className="raffleDets">Price per ticket: {ticketPrice}</p>
                 <p className="raffleDets">Raffle Start Date:<Moment>{raffleStartDate}</Moment></p>
                 <p className="raffleDets">Raffle End Date:<Moment>{raffleEndDate}</Moment></p>
-
                 <StripeCheckout
                   token={this.onToken.bind(this)}
                   stripeKey={keys.stripePublishableKey}
                   amount={ticketPrice * 100} />
-
-
-
-
               </div>
               <div className="col-md-1">
               </div>
             </div>
           </div>
         </div>
-
-
-
-
-
       </div>
-
-
-
     );
-    // }
-    // else {
-    //   return (
-    //     <div>
-    //     <p>Loading.......</p></div>
-    //   );
-    // }
   }
 }
-
-
 export default ViewItem;
